e62029cf7028ab96f2aa62937daa1653741c4876
schoetlr <srlrs20020@gmail.com>
2016-07-08 12:25:31 -0400

    testing add and subtract

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # assignment_rspec_calculator
-
+Luke/David Assignment
 01134
 
 [A Ruby and RSpec testing assignment from the Viking Code School](http://www.vikingcodeschool.com)
--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -1,1 +1,42 @@
-# Your code here
+require 'calculator'
+
+
+describe Calculator do 
+  # before(:each) do 
+  #   calc = Calculator.new
+  # end
+  let(:calc) {Calculator.new}
+
+  describe '#add' do 
+    it "adds positive numbers" do
+      expect(calc.add(4,5)).to eq(9)
+    end
+
+    it "adds negative numbers" do 
+      expect(calc.add(-1, -2)).to eq(-3)
+    end
+
+    it "adds floating point numbers" do 
+      expect(calc.add(0.54, 1.2)).to be_within(0.5).of (1.74)
+    end
+  end
+
+  describe '#subtract' do 
+    it "subtracts positive numbers" do 
+      expect(calc.subtract(5, 4)).to eq(1)
+    end
+
+    it "subtracts negative numbers" do 
+      expect(calc.subtract(-4, -5)).to eq(1)
+    end
+
+    it "subtracts floating point numbers" do 
+      expect(calc.subtract(0.55, 0.15)).to be_within(0.3).of (0.4)
+    end
+  end
+
+  describe '#multiply' do
+
+  end
+
+end
